3efb6fff230192a172a6116b0866946c1936fb32
San-FG <manuelsal5436-gmail.com>
2025-11-02 14:44:04 -0500

    Ingreso de las funciones insert e is_empty

--- DataStructures/Priority_queue/priority_queue.py
+++ DataStructures/Priority_queue/priority_queue.py
@@ -23,6 +23,14 @@ def new_heap(is_min_pq):
         new["cmp_function"] = default_compare_lower_value
     elif is_min_pq == False:
         new["cmp_function"] = default_compare_higher_value
+    new["elements"] = al.add_first(new["elements"],None)
     return new   
 
+def insert(my_heap,priority,value):
+    new_pq = pqe.new_pq_entry(priority,value)
+    my_heap["elements"] = al.add_last(my_heap["elements"],new_pq)
+    " Funcion a implementar /swim(my_heap)"
+    return my_heap
 
+def is_empty(my_heap):
+    return my_heap["size"] == 0 
